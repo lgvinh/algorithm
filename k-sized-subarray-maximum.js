b9e3bf0eea4b66d65349bec054d07295008ad9ed
Vinh Lam <vinh.lam@zoi.de>
2025-04-01 10:30:00 +0700

    fixed k-sized-subarray

--- k-sized-subarray-maximum.js
+++ k-sized-subarray-maximum.js
@@ -1,21 +1,29 @@
 const maxOfSubarrays = (arr, k) => {
   const result = [];
+  const deque = []; // Stores indices of array elements
 
-  for (let i = 0; i < arr.length - k + 1; i++) {
-    let maximum = arr[i];
-    for (let j = i + 1; j < i + k; j++) {
-      const element = arr[j];
-      
-      if (element > maximum) {
-        maximum = element;
-      }
+  for (let i = 0; i < arr.length; i++) {
+    // Remove indices that are out of the current window
+    if (deque.length > 0 && deque[0] < i - k + 1) {
+      deque.shift();
     }
 
-    result.push(maximum);
+    // Remove indices of elements smaller than the current element
+    // as they will not be the maximum in this window
+    while (deque.length > 0 && arr[deque[deque.length - 1]] < arr[i]) {
+      deque.pop();
+    }
+
+    // Add the current element's index to the deque
+    deque.push(i);
+
+    // Add the maximum element of the current window to the result
+    if (i >= k - 1) {
+      result.push(arr[deque[0]]);
+    }
   }
 
   return result;
 }
-
 
 console.log(maxOfSubarrays([1, 2, 3, 1, 4, 5, 2, 3, 6], 3)); // 3 3 4 5 5 5 6
